72c90796bf8d59c32d7ce1022b4b553b59797a75
jffryi <jefferyi.magtubo@gmail.com>
2024-03-01 16:02:09 -0600

    removed parenthesis from function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function updateValue() {
     output.textContent = slider.value;
 }
 
-document.getElementById("slider").addEventListener('input', updateValue())
+document.getElementById("slider").addEventListener('input', updateValue)
 
 function updateNumbers() {
     let numbers = document.getElementById("numbers")
